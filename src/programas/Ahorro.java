9d3ef328724aea2d716f29d0ffa9e6696df1bb82
BOT-Frank <sgsagobpe@gmail.com>
2026-01-06 21:32:10 -0500

    TERCER comit

--- src/programas/Ahorro.java
+++ src/programas/Ahorro.java
@@ -11,5 +11,6 @@ package programas;
 public class Ahorro {
     public static void main (String[] args){
     System.out.println("Ahorro mensual");
+    System.out.println("Ahorro semestral");
     }
 }
